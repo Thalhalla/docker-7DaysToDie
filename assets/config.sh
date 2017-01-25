71cb1f277b38af78f0b3ce89ed4cd9bca73de3f8
James S. Moore <james@webtechhq.com>
2017-01-25 17:35:34 -0600

    More updates for STEAM_FORCE_INSTALL

--- assets/config.sh
+++ assets/config.sh
@@ -2,8 +2,5 @@
 
 cd $STEAM_FORCE_INSTALL
 
-cp /assets/cfg/serveradmin_template.xml ./
-cp /assets/cfg/serverconfig_template.xml ./
-
 sed -i "s/REPLACEME_CONTROLPANEL_PASSWORD/$REPLACEME_CONTROLPANEL_PASSWORD/" ./serverconfig_template.xml
 sed -i "s/REPLACEME_TELNET_PASSWORD/$REPLACEME_TELNET_PASSWORD/" ./serverconfig_template.xml
--- assets/valve-install.sh
+++ assets/valve-install.sh
@@ -2,12 +2,12 @@
 
 sudo chown -R steam:steam /home/steam
 mkdir -p $STEAM_FORCE_INSTALL
-cp /assets/steamer.txt /home/steam/
-cd /home/steam
+cp /assets/steamer.txt $STEAM_FORCE_INSTALL
+cd $STEAM_FORCE_INSTALL
 sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
 sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
 sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
 sed -i "s/REPLACEME_FORCE_INSTALL/$STEAM_FORCE_INSTALL/" steamer.txt
 
-./steamcmd.sh +runscript /home/steam/steamer.txt
-sudo chown -R steam:steam /home/steam
+./steamcmd.sh +runscript $STEAM_FORCE_INSTALL/steamer.txt
+sudo chown -R steam:steam $STEAM_FORCE_INSTALL
