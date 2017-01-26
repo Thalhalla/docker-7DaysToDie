0073dcf5c7a0a855112198c84e6cbb9d86f49a51
James S. Moore <james@webtechhq.com>
2017-01-26 12:22:26 -0600

    revert to original method for steamer.txt and allow proc to hang open

--- assets/valve-install.sh
+++ assets/valve-install.sh
@@ -2,23 +2,14 @@
 
 sudo chown -R steam:steam /home/steam
 mkdir -p $STEAM_FORCE_INSTALL
-# cp /assets/steamer.txt $STEAM_FORCE_INSTALL
+cp /assets/steamer.txt $STEAM_FORCE_INSTALL
 
 cd $STEAM_FORCE_INSTALL
-# original method
-# sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
-# sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
-# sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
-# sed -i "s/REPLACEME_FORCE_INSTALL/$STEAM_FORCE_INSTALL/" steamer.txt
 
-# alternate method
-rm ./steamer.txt
-echo "@ShutdownOnFailedCommand 1" > ./steamer.txt
-echo "@NoPromptForPassword 1" >> ./steamer.txt
-echo "login $STEAM_USERNAME $STEAM_PASSWORD" >> ./steamer.txt
-echo "force_install_dir $STEAM_FORCE_INSTALL" >> ./steamer.txt
-echo "app_update 294420" >> ./steamer.txt
-echo "quit" >> ./steamer.txt
+sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
+sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
+sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
+sed -i "s/REPLACEME_FORCE_INSTALL/$STEAM_FORCE_INSTALL/" steamer.txt
 
 $STEAM_CMD_PATH/steamcmd.sh +runscript $STEAM_FORCE_INSTALL/steamer.txt
 sudo chown -R steam:steam $STEAM_FORCE_INSTALL
--- assets/valve-run.sh
+++ assets/valve-run.sh
@@ -5,5 +5,3 @@
 cd $STEAM_FORCE_INSTALL
 echo "-= Starting 7 Days to Die Dedicated Server =-"
 ./7DaysToDieServer.x86_64 -logfile 7DaysToDieServer_Data/output_log.txt -quit -batchmode -nographics -dedicated -configfile=serverconfig.xml
-tail -f 7DaysToDieServer_Data/output_log.txt
-sleep 3600
