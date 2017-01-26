5bf6b00224861086a1df4c9112f5f564eccf87a2
James S. Moore <james@webtechhq.com>
2017-01-25 21:13:11 -0600

    Update to valve method

--- assets/valve-run.sh
+++ assets/valve-run.sh
@@ -3,12 +3,10 @@
 /bin/bash /assets/config.sh
 
 cd $STEAM_FORCE_INSTALL
-./sdtdserver start
-echo "started server"
-sleep 10
-echo "retreiving details"
-sleep 3
-./sdtdserver details
-sleep 300
-# infinite loop to keep it open for Docker
-while true; do ./sdtdserver monitor; sleep 300; done
+while [ 1 ]
+do
+    ./startserver.sh -configfile=serverconfig.xml
+    sleep 3
+    echo "-= Restarting 7 Days to Die Dedicated Server =-"
+    sleep 3
+done
