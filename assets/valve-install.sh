e0d68d7536c6bf2e5f3169b11d6041ce6095f926
James S. Moore <james@webtechhq.com>
2017-01-26 12:59:27 -0600

    Fix for slashes in STEAM_FORCE_INSTALL

--- assets/valve-install.sh
+++ assets/valve-install.sh
@@ -9,7 +9,7 @@ cd $STEAM_FORCE_INSTALL
 sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
 sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
 sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
-sed -i "s/REPLACEME_FORCE_INSTALL/$STEAM_FORCE_INSTALL/" steamer.txt
+sed -i "s!REPLACEME_FORCE_INSTALL!$STEAM_FORCE_INSTALL!" steamer.txt
 
 $STEAM_CMD_PATH/steamcmd.sh +runscript $STEAM_FORCE_INSTALL/steamer.txt
 sudo chown -R steam:steam $STEAM_FORCE_INSTALL
