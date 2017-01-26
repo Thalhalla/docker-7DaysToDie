4444abce5583a5368546e7f7a77574f4fd810df2
James S. Moore <james@webtechhq.com>
2017-01-26 16:23:07 -0600

    enable logging tail for debugging

--- assets/valve-run.sh
+++ assets/valve-run.sh
@@ -4,4 +4,6 @@
 
 cd $STEAM_FORCE_INSTALL
 echo "-= Starting 7 Days to Die Dedicated Server =-"
+touch 7DaysToDieServer_Data/output_log.txt
 ./7DaysToDieServer.x86_64 -logfile 7DaysToDieServer_Data/output_log.txt -quit -batchmode -nographics -dedicated -configfile=serverconfig.xml
+tail -f 7DaysToDieServer_Data/output_log.txt
