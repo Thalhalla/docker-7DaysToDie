7fd6cc344fdd923d8e99a2e4d9650cf69d7bc849
James S. Moore <james@webtechhq.com>
2017-01-25 20:51:53 -0600

    take 2

--- assets/valve-install.sh
+++ assets/valve-install.sh
@@ -12,12 +12,12 @@ cd $STEAM_FORCE_INSTALL
 # sed -i "s/REPLACEME_FORCE_INSTALL/$STEAM_FORCE_INSTALL/" steamer.txt
 
 # alternate method
-echo '@ShutdownOnFailedCommand 1' > ./steamer.txt
-echo '@NoPromptForPassword 1' >> ./steamer.txt
-echo 'login '.$STEAM_USERNAME.' '.$STEAM_PASSWORD >> ./steamer.txt
-echo 'force_install_dir '.$STEAM_FORCE_INSTALL >> ./steamer.txt
-echo 'app_update '.$STEAM_GID >> ./steamer.txt
-echo 'quit' >> ./steamer.txt
+echo "@ShutdownOnFailedCommand 1" > ./steamer.txt
+echo "@NoPromptForPassword 1" >> ./steamer.txt
+echo "login $STEAM_USERNAME $STEAM_PASSWORD" >> ./steamer.txt
+echo "force_install_dir $STEAM_FORCE_INSTALL" >> ./steamer.txt
+echo "app_update $STEAM_GID" >> ./steamer.txt
+echo "quit" >> ./steamer.txt
 
 $STEAM_CMD_PATH/steamcmd.sh +runscript $STEAM_FORCE_INSTALL/steamer.txt
 sudo chown -R steam:steam $STEAM_FORCE_INSTALL
