6601e3f07f4b1fbdd9d51164277d728750c1971f
James S. Moore <james@webtechhq.com>
2017-01-25 23:28:42 -0600

    re-add sleep timer for debug

--- assets/valve-run.sh
+++ assets/valve-run.sh
@@ -6,3 +6,4 @@ cd $STEAM_FORCE_INSTALL
 echo "-= Starting 7 Days to Die Dedicated Server =-"
 ./7DaysToDie.x86 -logfile 7DaysToDie_Data/output_log.txt -quit -batchmode -nographics -configfile=serverconfig.xml -dedicated
 tail -f 7DaysToDie_Data/output_log.txt
+sleep 3600
