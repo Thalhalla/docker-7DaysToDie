076201666c41dde4358d6027b9906acd1fbebc51
James S. Moore <james@webtechhq.com>
2017-01-25 23:46:36 -0600

    valve-run updates

--- assets/valve-run.sh
+++ assets/valve-run.sh
@@ -4,6 +4,6 @@
 
 cd $STEAM_FORCE_INSTALL
 echo "-= Starting 7 Days to Die Dedicated Server =-"
-/bin/bash ./7DaysToDie.x86_64 -logfile 7DaysToDie_Data/output_log.txt -quit -batchmode -nographics -dedicated -configfile=serverconfig.xml
-tail -f 7DaysToDie_Data/output_log.txt
+/bin/bash ./7DaysToDieServer.x86_64 -logfile 7DaysToDieServer_Data/output_log.txt -quit -batchmode -nographics -dedicated -configfile=serverconfig.xml
+tail -f 7DaysToDieServer_Data/output_log.txt
 sleep 3600
