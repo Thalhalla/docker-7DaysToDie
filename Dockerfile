2e54b201cc86c3b48f8583b391c477642b747f1a
James S. Moore <james@webtechhq.com>
2017-02-14 19:37:24 -0600

    -nographics, libglu libxcursor libxrandr

--- Dockerfile
+++ Dockerfile
@@ -19,7 +19,7 @@ chown -R steam. /home/steam
 # Install frame buffer via non-interactive apt
 ENV DEBIAN_FRONTEND noninteractive
 RUN apt-get update && \
-apt-get install -yqq xvfb && \
+apt-get install -yqq xvfb libglu libxcursor libxrandr && \
 rm -rf /var/lib/apt/lists/*
 # End non-interactive apt
 ENV DEBIAN_FRONTEND interactive
--- assets/valve-run.sh
+++ assets/valve-run.sh
@@ -9,5 +9,5 @@ sudo Xvfb &
 cd $STEAM_FORCE_INSTALL
 echo "-= Starting 7 Days to Die Dedicated Server =-"
 touch 7DaysToDieServer_Data/output_log.txt
-./7DaysToDieServer.x86_64 -logfile 7DaysToDieServer_Data/output_log.txt -quit -batchmode -nographics -dedicated -configfile=serverconfig.xml
+./7DaysToDieServer.x86_64 -logfile 7DaysToDieServer_Data/output_log.txt -quit -batchmode -dedicated -configfile=serverconfig.xml &
 tail -f 7DaysToDieServer_Data/output_log.txt
