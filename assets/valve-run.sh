34108d5ff88e39c2125ba37eec4112e06aa283b7
Josh Cox <josh@webhosting.coop>
2018-12-28 13:38:44 -0600

    data

--- assets/valve-run.sh
+++ assets/valve-run.sh
@@ -6,7 +6,8 @@ sudo Xvfb &
 # perform configuration of this server instance
 /bin/bash /assets/config.sh
 
-cd $STEAM_FORCE_INSTALL
+#cd $STEAM_FORCE_INSTALL
+cd /data
 echo "-= Starting 7 Days to Die Dedicated Server =-"
 mkdir -p 7DaysToDieServer_Data
 touch 7DaysToDieServer_Data/output_log.txt
