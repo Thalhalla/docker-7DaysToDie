34abfe969dddbdc73182575740e2c26dd87c2a93
Josh Cox <josh@webhosting.coop>
2018-12-28 09:44:46 -0600

    mkdir

--- assets/valve-run.sh
+++ assets/valve-run.sh
@@ -8,6 +8,7 @@ sudo Xvfb &
 
 cd $STEAM_FORCE_INSTALL
 echo "-= Starting 7 Days to Die Dedicated Server =-"
+mkdir -p 7DaysToDieServer_Data
 touch 7DaysToDieServer_Data/output_log.txt
 ./7DaysToDieServer.x86_64 -logfile 7DaysToDieServer_Data/output_log.txt -quit -batchmode -dedicated -configfile=serverconfig.xml &
 tail -f 7DaysToDieServer_Data/output_log.txt
